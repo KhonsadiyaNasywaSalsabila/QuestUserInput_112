2451fed4421ab3e3cb97a34fc113455960046d4a
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-10-30 16:29:19 +0700

    Menambahkan RadioButton untuk pemilihan gender.

--- app/src/main/java/com/example/praktikum5/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/TeksEdit.kt
@@ -61,9 +61,24 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
                 textNama = it
             },
         )
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier
+                        .selectable(
+                            selected = textJK == item,
+                            onClick = { textJK = item }
+                        ), verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = { textJK = item }
+                    )
+                    Text(item)
+                }
+            }
 
 
-
-
+        }
     }
 }

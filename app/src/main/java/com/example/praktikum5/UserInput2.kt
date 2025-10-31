ebebd4edaa8740071742798679ffa8313032ba73
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-01 05:38:32 +0700

    Menambahkan Column yang akan menampung semua elemen input

--- app/src/main/java/com/example/praktikum5/UserInput2.kt
+++ app/src/main/java/com/example/praktikum5/UserInput2.kt
@@ -75,5 +75,13 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
                 .background(purpleHeader, shape = RoundedCornerShape(8.dp))
                 .padding(16.dp)
         )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .background(Color.White, shape = RoundedCornerShape(12.dp))
+                .padding(16.dp)
+        ) {}
     }
 }

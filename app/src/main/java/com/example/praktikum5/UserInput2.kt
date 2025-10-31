bb929e1e762548842ecbfa1ddff2d9f54b883e46
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-01 06:03:05 +0700

    Input Field: Jenis Kelamin

--- app/src/main/java/com/example/praktikum5/UserInput2.kt
+++ app/src/main/java/com/example/praktikum5/UserInput2.kt
@@ -106,6 +106,36 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
                     unfocusedBorderColor = Color.LightGray
                 )
             )
+            Text(
+                text = "JENIS KELAMIN",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 20.dp, bottom = 8.dp)
+            )
+            Column {
+                gender.forEach { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            )
+                            .padding(vertical = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = textJK == item,
+                            onClick = {
+                                textJK = item
+                            }
+                        )
+                        Text(text = item, modifier = Modifier.padding(start = 8.dp))
+                    }
+                }
+            }
+
         }
     }
 }

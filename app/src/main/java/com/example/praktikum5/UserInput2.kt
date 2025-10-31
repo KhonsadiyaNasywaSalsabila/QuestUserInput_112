8c13e5ecac023e1d3d91c087e06f9b84f52c9a1a
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-01 05:08:10 +0700

    State untuk data yang akan ditampilkan di dialog (setelah disubmit)

--- app/src/main/java/com/example/praktikum5/UserInput2.kt
+++ app/src/main/java/com/example/praktikum5/UserInput2.kt
@@ -43,6 +43,11 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
 
 
 }

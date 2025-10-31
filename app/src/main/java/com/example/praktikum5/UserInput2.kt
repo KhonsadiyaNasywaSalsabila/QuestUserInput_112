bb98a4541d6ac54c3018784e415fc2dfe8c8f7d0
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-01 05:22:21 +0700

    Mendefinisikan konstanta warna

--- app/src/main/java/com/example/praktikum5/UserInput2.kt
+++ app/src/main/java/com/example/praktikum5/UserInput2.kt
@@ -53,4 +53,7 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
     val statusPerkawinan: List<String> = listOf("Janda", "Lajang", "Duda")
 
+    val purpleBackground = Color(0xFFE6D5F5)
+    val purpleButton = Color(0xFF7C3AED)
+    val purpleHeader = Color(0xFFB794F6)
 }
